c3d9b7f16caae1d147d286b4f6822da7d3ceda76
gcpd88 <gcpd88@gmail.com>
2025-09-28 05:17:41 -0400

    04_leds.asm

--- 04_Machine_Language/leds.asm
+++ 04_Machine_Language/leds.asm
@@ -2,4 +2,12 @@
 // execute an infinite loop to
 // read the button state and output to led
 
-// Put your code here:
+(LOOP)
+@BUT // store button state (inverted)
+D=!M  // 0 = pushed down, 1 = released
+
+@LED // update LED state
+M=D // 0 = LED off, 1 = LED on
+
+@LOOP
+0;JMP
