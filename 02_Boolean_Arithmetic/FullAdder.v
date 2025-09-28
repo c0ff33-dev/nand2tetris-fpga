a06dfc71939c26da7145e579c4065dcecb0c8647
gcpd88 <gcpd88@gmail.com>
2025-09-27 22:44:53 -0400

    02_FullAdder

--- 02_Boolean_Arithmetic/FullAdder.v
+++ 02_Boolean_Arithmetic/FullAdder.v
@@ -4,13 +4,14 @@
 
 `default_nettype none
 module FullAdder(
-	input a,		//1-bit input
-	input b,		//1-bit input
-	input c,		//1-bit input
-	output sum,		//Right bit of a + b + c
-	output carry	//Left bit of a + b + c
+	input a,		// 1-bit input
+	input b,		// 1-bit input
+	input c,		// 1-bit input
+	output sum,		// Right bit of a + b + c
+	output carry	// Left bit of a + b + c
 );
 
-	// Put your code here:
+	assign sum  = a ^ b ^ c;
+	assign carry = (a & b) | (b & c) | (a & c);
 
 endmodule
