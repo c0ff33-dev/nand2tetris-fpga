369a3434741e584653534d1416c64089f16d47bc
gcpd88 <gcpd88@gmail.com>
2025-09-30 02:02:59 -0400

    05/03_Clock25 (pending reset)

--- 05_Computer_Architecture/Clock25_Reset20.v
+++ 05_Computer_Architecture/Clock25_Reset20.v
@@ -10,6 +10,13 @@ module Clock25_Reset20(
 	output reset 		// reset signal approx. 20us
 );
 
-	// Put your code here:
+	// scale down 100MHz to 25MHz (1/4)
+	// 2 bits = 2^2 = 4 ticks x 2 per cycle = 2^3 for 4 cycles
+	wire [15:0] psout;
+	PC prescaler(.clk(CLK),.load(1'b0),.in(16'b0),.reset(1'b0),.inc(1'b1),.out(psout));
+	Buffer clock(.in(psout[2]),.out(clk)); // demux the 3rd bit
+
+	// TODO
+	// assign reset = 1'b0;
 
 endmodule
