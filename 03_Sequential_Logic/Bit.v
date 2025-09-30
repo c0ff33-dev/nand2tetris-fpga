2e62f751bf1bb0041777edc5e0c92881848c3559
gcpd88 <gcpd88@gmail.com>
2025-09-30 01:54:31 -0400

    Fix Bit wiring

--- 03_Sequential_Logic/Bit.v
+++ 03_Sequential_Logic/Bit.v
@@ -12,18 +12,13 @@ module Bit(
 	output out // consumers of the DFF can use a wire though
 );
 
-	wire dff_in;
-    wire dff_out;
-
-    // Multiplexer for load functionality:
+	// Multiplexer for load functionality:
     // If load=1, feed 'in' to DFF,
 	// else feed previous output to DFF to hold state
-    assign dff_in = load ? in : dff_out;
-
     DFF dff (
         .clk(clk),
-        .in(dff_in),
-        .out(dff_out)
+        .in(load ? in : out),
+        .out(out)
     );
 
 endmodule
