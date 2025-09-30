e2bb7e54c4e51e1b282daf0482d5f6d24de643c8
gcpd88 <gcpd88@gmail.com>
2025-09-30 04:45:49 -0400

    05/04_HACK (initial)

--- 05_Computer_Architecture/ROM.v
+++ 05_Computer_Architecture/ROM.v
@@ -1,7 +1,7 @@
 /**
  * instruction memory at boot time 
  * The instruction memory is read only (ROM) and
- * preloaded with 256 x 16bit of Hackcode holding the bootloader.
+ * preloaded with 256 x 16bit of Hack code holding the bootloader.
  * 
  * instruction = ROM[pc]
  */
@@ -13,11 +13,16 @@ module ROM(
 );
 
 	// No need to implement this chip
-	// The file ROM.BIN holds the hack code 
+	// ROM.hack is an ASCII encoded version of the binary
+	// $readmemb is only used in simulation / will implicitly decode
+
+	// ROM.bin is the raw binary uploaded with iceprogduino
+	// `hexdump -C` will show the canonical representation of the binary
+	// (after each line has been converted to int and split into 2 bytes)
 	parameter ROMFILE = "ROM.hack";
 	
 	reg [15:0] mem [0:255];
-	assign instruction = mem[pc[7:0]];
+	assign instruction = mem[pc[7:0]]; // read in one byte?
 	
 	initial begin
 		$readmemb(ROMFILE,mem);
