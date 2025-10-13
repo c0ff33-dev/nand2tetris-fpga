708576f9b47f7c2b49105a128eadd6b8c6093455
gcpd88 <gcpd88@gmail.com>
2025-10-12 23:13:24 -0400

    SPI: timing (still not working)

--- 06_IO_Devices/SPI.v
+++ 06_IO_Devices/SPI.v
@@ -63,24 +63,25 @@ module SPI(
 		.out(clkCount)
 	);
 
-	assign reset = (clkCount == 16'd15); // TODO: shouldn't this reset after 8 clock cycles?
+	assign reset = (clkCount == 16'd15);
 
-	// miso = SDI in [t+1]
+	// sample MISO at posedge of SCK
 	// clk domain in spi_tb
 	always @(posedge clk)
-		miso <= SDI;
+		// clkCount[0] = odd cycles (SCK=1)
+		if (clkCount[0] & clkCount!=0) 
+        	miso <= SDI;
 
 	// circular buffer to enable duplex comms with slave where:
 	// slave MSB >= master LSB (MISO)
 	// master MSB >= slave LSB (MOSI)
-	// init=0 before initial load
-	// shift on SCK falling edge (not clk which is 2x faster)
+	// shift on negedge of SCK
 	BitShift8L shiftreg (
 		.clk(clk), // needs to be on clk domain for load
 		.in(init ? in[7:0] : 8'd0), // init on load
 		.inLSB(init ? miso : 1'b0), // shift slaveMSB into masterLSB while sampling
 		.load(init ? load : 1'b1), // don't shift on load
-		.shift(SCK & clkCount[0]), // prime SCK posedge so it shifts on SCK negedge
+		.shift(~clkCount[0] & clkCount!=0), // ~clkCount[0] = even cycles (SCK=0)
 		.out(shift)
 	);
 
@@ -92,11 +93,10 @@ module SPI(
 		end
 	end
 
-	// spi_tb requires SDO to transmit from the preceding SCK low which looks a bit strange but shouldn't effect sampling
-	// FIXME: hack_tb SDI waveform looks a bit funky as well and is shifting on SCK posedge not negedge
+	// FIXME: waveform looks better but spi_tb & hw fails
 	assign CSX = (init & CDONE) ? csx : 1'b1; // init CSX=1 as well
-	assign SDO = shift[7]; // MOSI (masterMSB to slaveLSB)
-	assign SCK = init ? (busy & clkCount[0]) : 1'b0; // run SCK while busy, half speed // TODO: why half speed?
+	assign SDO = busy & shift[7]; // MOSI (masterMSB to slaveLSB)
+	assign SCK = init ? (busy & clkCount[0]) : 1'b0; // run SCK while busy, half speed
 	assign out = {busy,7'd0,shift}; // out[15]=busy, out[7:0]=received byte
 
 endmodule
